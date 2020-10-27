536f4e043bfb24ccf2b5677201000e6b3d4c2db5
Guillermo Agudelo <guille.agudelo@gmail.com>
2020-10-27 16:46:09 -0500

    otro fix

--- resources/views/sections/contacto.blade.php
+++ resources/views/sections/contacto.blade.php
@@ -3,7 +3,7 @@
   <div>
     <h2 class="font-bold text-5xl lg:text-6xl">CONTÁCTENOS</h2>
     <p class="text-2xl mb-6">Trabajemos juntos para llevar su empresa al ciberespacio. Sus clientes lo están buscando en la web y nosotros los ayudaremos a encontrarlo.</p>
-    <p><a href="https://wa.me/573017529473" target="_blank" class="text-2xl flex items-center">@mdi('chat', 'fill-current') 301 752 0473</a></p>
+    <p><a href="https://wa.me/573017529473" target="_blank" class="text-2xl flex items-center">@mdi('chat', 'fill-current') +57 301 752 0473</a></p>
     <p><a href="mailto:contacto@doreaweb.ml" class="text-2xl flex items-center">@mdi('email', 'fill-current') contacto@doreaweb.ml</a></p>
   </div>
 
